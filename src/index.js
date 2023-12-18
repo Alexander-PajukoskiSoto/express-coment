da442a96245e981f9eb587ad8b15c42ed759cf44
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-12-18 10:33:11 +0100

    hehehaha

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.json('res')
+})
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
