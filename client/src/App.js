44d273f0b330866342648a72bf2314800c7eaf54
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2024-01-04 11:09:51 +0100

    alot (don't remember but still working)

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState([]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      // .then((data)=> data.forEach((element) => {
+      //   setData(data.title += element.title)
+      // }))
+      .then((data) => setData(data))
   }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {!data?'loading...':data}
+          {data.map((element, index) => (
+          <span key={index}>{element.title}</span>
+        ))}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form method='post' action='/createUser'>
+            <input type='text' id='createUsername' name='createUsername' placeholder='Username'>
+            </input>
+            <input type='text' id='email' name='email' placeholder='email'>
+            </input>
+            <input type='password' id='createPassword' name='createPassword' placeholder='password'>
+            </input>
+            <input type='submit'></input>
+        </form>
+        <form method='post' action='/login'>
+            <input type='text' id='username' name='username' placeholder='Username'>
+            </input>
+            <input type='password' id='password' name='password' placeholder='password'>
+            </input>
+            <input type='submit'></input>
+        </form>
+        <form method='post' action='/comment'>
+            <textarea id='content' name='content'>
+            </textarea>
+            <input type='submit'></input>
+        </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const express = require("express");
 const { PrismaClient } = require('@prisma/client');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
 
 const prisma = new PrismaClient();
 
@@ -8,11 +12,66 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const day = 1000 * 60 * 60 * 24;
+
+app.use(session({
+  secret: 'some secret',
+  cookie: {maxAge: day},
+  saveUninitialized: false,
+  resave: false
+}))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.use(express.static(path.resolve(__dirname,'../client/build')));
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get("/api", async(req, res) => {
+  const posts = await prisma.Post.findMany()
+  res.json(posts)
 });
+app.post('/login',async(req,res)=>{
+
+  const user = await prisma.User.findFirst({
+    where: {
+      OR:[{
+        name: req.body.username},
+      ]
+
+  }})
+    if(req.body.username == '')
+    {
+      res.redirect('/')
+    }else if(req.body.password ==  user.password ){
+      console.log('successfully logged in')
+      req.session.authenticated = true;
+      res.redirect('/')
+      console.log(req.session)
+    }
+    else{
+      res.redirect('/')
+    }
+})
+
+app.post('/comment', async(req, res)=>{
+  console.log(req.body.content);
+  res.redirect('/')
+})
+app.post('/createUser', async(req, res)=>{
+  try {
+    const user = await prisma.User.create({
+      data: {
+        name: req.body.createUsername,
+        email: req.body.email,
+        password: req.body.createPassword
+      }
+    })
+    console.log('created user')
+    res.redirect('/posts')
+    } catch (error) {
+      console.log(error);
+    }
+    
+  })
 app.get('*', (req,res)=>{
   res.sendFile(path.resolve(__dirname,'../client/build', 'index.html'));
 })
